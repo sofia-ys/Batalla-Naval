6a734f8c091acad7066fcae51b4b1e8d9eda06d6
AmedeoSandrucci <amedeo.sandrucci@icloud.com>
2025-10-21 14:27:38 -0300

    INitial file

---
+++ Parcial 2.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+#We start working on the classes:
+#class ship:
+'''     def __init__(self,name,size):
+        self.name = name
+        self.size = size
+        self.position = []
+         '''
+def create_matrix():
+    empty_in_grid = np.zeros(10,10)
+    
+    
+    
+    
+    
+class board:
+class 
+    
+
+
+def main():
+    while True:
+        #start menu with "Do you want to play?" and the possible answers are Yes or No""
+        answer = input("Do you want to play a game? (yes/no): ").strip().lower()
+        if answer == 'yes':
+            #refer to a class
+            
+        elif answer == 'no':
+            print("The game will not start")
+            break
+        else:
+            print("Invalid input, please try again")
+            
+if __name__ == '__main__':
+    main()
